7da4b3d6c0f030f37f865c82b58eade22011a8bd
ManApart <r@austinkucera.com>
2022-09-09 08:26:29 -0400

    work towards single token

--- src/main/kotlin/simpleSite/codeBlock/CodeBlockFormatter.kt
+++ src/main/kotlin/simpleSite/codeBlock/CodeBlockFormatter.kt
@@ -1,11 +1,29 @@
 package simpleSite.codeBlock
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
+
+private val tokenTypes = listOf(
+    TokenType("hljs-keyword", ExactMatcher("class"))
+)
 
 fun formatCodeBlocks(htmlText: String): String {
     val doc = Jsoup.parse(htmlText)
-    val codeElements = doc.select("code")
-    codeElements.forEach { it.addClass("hljs") }
+    doc.select("code").forEach { formatCodeBlock(it) }
 
     return doc.html()
+}
+
+private fun formatCodeBlock(block: Element) {
+    block.addClass("hljs")
+    var html = block.html()
+    getTokens(block.html())
+        .sortedByDescending { it.start }
+        .forEach { html = it.embellish(html) }
+
+    block.html(html)
+}
+
+private fun getTokens(block: String): List<Token> {
+    return tokenTypes.flatMap { it.parse(block) }
 }
--- src/main/kotlin/simpleSite/codeBlock/Identifier.kt
+++ src/main/kotlin/simpleSite/codeBlock/Identifier.kt
@@ -4,14 +4,15 @@ interface Matcher {
     fun getNext(line: String, start: Int): Pair<Int, Int>?
 }
 
-class Cap(val starts: List<String>, val ends: List<String>, val includeStart: Boolean = false, val includeEnd: Boolean = false){
-    constructor(start: String, end: String, includeStart: Boolean = false, includeEnd: Boolean = false): this(listOf(start), listOf(end), includeStart, includeEnd)
+class Cap(val starts: List<String>, val ends: List<String>, val includeStart: Boolean = false, val includeEnd: Boolean = false) {
+    constructor(start: String, end: String, includeStart: Boolean = false, includeEnd: Boolean = false) : this(listOf(start), listOf(end), includeStart, includeEnd)
+
     fun getNext(line: String, start: Int): Pair<Int, Int>? {
         return null
     }
 }
 
-class Identifier(val caps: List<Cap>) : Matcher {
+class Identifier(private val caps: List<Cap>) : Matcher {
     override fun getNext(line: String, start: Int): Pair<Int, Int>? {
         //find all matching caps
         //return the one with the earliest start
@@ -20,24 +21,27 @@ class Identifier(val caps: List<Cap>) : Matcher {
     }
 }
 
-class ExactMatcher(private val match: String) : Matcher {
+class ExactMatcher(private val matches: List<String>) : Matcher {
+    constructor(match: String) : this(listOf(match))
+
     override fun getNext(line: String, start: Int): Pair<Int, Int>? {
-        val matchStart = line.indexOf(match, start)
-        val end = matchStart + match.length
-        return if (matchStart != -1 && end != -1) Pair(matchStart, end) else null
+        return matches.mapNotNull { match ->
+            val matchStart = line.indexOf(match, start)
+            val end = matchStart + match.length
+            if (matchStart != -1 && end != -1) Pair(matchStart, end) else null
+        }.minByOrNull { it.first }
     }
 }
 
 class TokenType(className: String, matcher: Matcher) {
-    fun parse(line: String): List<Token>{
+    fun parse(line: String): List<Token> {
         //keep calling getNext until it returns empty
         return emptyList()
     }
 }
 
 //These are coordinates of the OG string, but will be wrong once we manipulate it
-//Carry offset for how much longer the string is?
-//Will work as long as there is not string offset
+//Must be called working from back of the string towards the front
 class Token(val start: Int, val end: Int, val className: String) {
     fun embellish(line: String): String {
         return line
